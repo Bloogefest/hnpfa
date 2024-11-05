af86d08a1e0d0e9e1992d9c29d36d469d7dea5f5
Bloogefest <bloogefest@gmail.com>
2024-11-05 22:18:21 +0300

    [Server] Edit ServerApplication

--- server/src/main/java/ru/hnpfa/server/ServerApplication.java
+++ server/src/main/java/ru/hnpfa/server/ServerApplication.java
@@ -2,11 +2,18 @@ package ru.hnpfa.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.lang.NonNull;
 
+/**
+ * @since 1.0.0-RC1
+ */
 @SpringBootApplication
-public class ServerApplication {
+class ServerApplication {
 
-    public static void main(String[] args) {
+    /**
+     * @since 1.0.0-RC1
+     */
+    public static void main(final @NonNull String... args) {
         SpringApplication.run(ServerApplication.class, args);
     }
 
